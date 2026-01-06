b8715afb5ac4def8b4980cdd83fa6fe1e18a96da
BMM Operator <xf06bm@xf06bm-ws3.nsls2.bnl.local>
2026-01-06 13:45:26 -0500

    forgot the __init__.py file

---
+++ src/bmm_tools/__init__.py
@@ -0,0 +1,11 @@
+"""
+Copyright (c) 2026 Bruce Ravel. All rights reserved.
+
+bmm_tools: Common code for Bluesky profiles for BMM end stations and other tools
+"""
+
+from __future__ import annotations
+
+__version__ = "0.1.0"
+
+__all__ = ["__version__"]
